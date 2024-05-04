d900811e768f2116835324cf02f92fb80bf81208
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-05-04 17:15:33 +0530

    Update leaderboard.py

--- Vizxer-m/Modules/leaderboard.py
+++ Vizxer-m/Modules/leaderboard.py
@@ -5,11 +5,11 @@ import html
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext
 
-from shivu import (application, PHOTO_URL, OWNER_ID,
+from Vizxer-m import (application, PHOTO_URL, OWNER_ID,
                     user_collection, top_global_groups_collection, top_global_groups_collection, 
                     group_user_totals_collection)
 
-from shivu import sudo_users as SUDO_USERS 
+from Vizxer-m import sudo_users as SUDO_USERS 
 
     
 async def global_leaderboard(update: Update, context: CallbackContext) -> None:
