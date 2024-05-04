69b09258fc67e564ee32839f84b7cf428d2f7ba2
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-05-04 18:27:12 +0530

    Update config.py

--- Vizxer/config.py
+++ Vizxer/config.py
@@ -11,7 +11,7 @@ class Config(object):
     SUPPORT_CHAT = "Rulers_Bots_Support"
     UPDATE_CHAT = "waifuobtainers"
     BOT_USERNAME = "Waifu_obtainerbot"
-    CHARA_CHANNEL_ID = "-1002145420408"
+    CHARA_CHANNEL_ID = "-1002071072596"
     api_id = 19869418
     api_hash = "c69a930758c50d764ad678b84c097a55"
 
