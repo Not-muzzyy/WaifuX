dd7b2dfb5dde9dfaa85bc7dc22af475717f05085
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-05-04 18:25:18 +0530

    Update config.py

--- Vizxer/config.py
+++ Vizxer/config.py
@@ -5,13 +5,13 @@ class Config(object):
     OWNER_ID = "6453545159"
     sudo_users = "7011799629", "6396361171"
     GROUP_ID = -1002145420408
-    TOKEN = "6889782542:AAFfQ13Rji_eDZkysaxHeEJTWdfjsDuNPts"
+    TOKEN = "7072961250:AAEbHVXWJnIJEkjpr5aBzKmkrQnLUze_4qQ"
     mongo_url = "mongodb+srv://edit72160:edit72160@cluster0.aceg9xy.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b8232e33e2e1d09c5687f.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "Rulers_Bots_Support"
     UPDATE_CHAT = "waifuobtainers"
     BOT_USERNAME = "Waifu_obtainerbot"
-    CHARA_CHANNEL_ID = "-1002071072596"
+    CHARA_CHANNEL_ID = "-1002145420408"
     api_id = 19869418
     api_hash = "c69a930758c50d764ad678b84c097a55"
 
