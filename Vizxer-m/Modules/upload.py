1f1c651e539bde771ce6bec864a2a330020bba09
Vizxer-m <132169076+Not-muzzyy@users.noreply.github.com>
2024-05-04 16:13:39 +0530

    Update upload.py
    Xd

--- Vizxer-m/Modules/upload.py
+++ Vizxer-m/Modules/upload.py
@@ -12,7 +12,7 @@ img_url character-name anime-name rarity-number
 
 use rarity number accordingly rarity Map
 
-rarity_map = 1 (⚪️ Common), 2 (🟣 Rare) , 3 (🟡 Legendary), 4 (🟢 Medium)"""
+rarity_map = 1 (🔘 Common), 2 (🟢 Uncommon) , 3 (🧿 Rare), 4 (🔮 Epic), 5 (🏵️ Legendary), 6 (🎴 Mythic), 7 (💎 Exiotic)"""
 
 
 
@@ -48,11 +48,11 @@ async def upload(update: Update, context: CallbackContext) -> None:
             await update.message.reply_text('Invalid URL.')
             return
 
-        rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium"}
+        rarity_map = {1: "🔘 Common", 2: "🟢 Uncommon", 3: "🧿 Rare", 4: "🔮 Epic", 5: "🏵️ Legendary", 6: "🎴 Mythic", 7: "💎 Exiotic"}
         try:
-            rarity = rarity_map[int(args[3])]
+            rarity = rarity_map[int(args[7])]
         except KeyError:
-            await update.message.reply_text('Invalid rarity. Please use 1, 2, 3, 4, or 5.')
+            await update.message.reply_text('Invalid rarity. Please use 1, 2, 3, 4, 5, 6 or 7')
             return
 
         id = str(await get_next_sequence_number('character_id')).zfill(2)
@@ -132,11 +132,11 @@ async def update(update: Update, context: CallbackContext) -> None:
         if args[1] in ['name', 'anime']:
             new_value = args[2].replace('-', ' ').title()
         elif args[1] == 'rarity':
-            rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium", 5: "💮 Special edition"}
+            rarity_map = {1: "🔘 Common", 2: "🟢 Uncommon", 3: "🧿 Rare", 4: "🔮 Epic", 5: "🏵️ Legendary", 6: "🎴 Mythic", 7: "💎 Exiotic"}
             try:
                 new_value = rarity_map[int(args[2])]
             except KeyError:
-                await update.message.reply_text('Invalid rarity. Please use 1, 2, 3, 4, or 5.')
+                await update.message.reply_text('Invalid rarity. Please use 1, 2, 3, 4,5,6 or 7')
                 return
         else:
             new_value = args[2]
