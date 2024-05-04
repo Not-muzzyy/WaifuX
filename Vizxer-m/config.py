207f782fce5aa453d8843465998c338dc0df737a
Vizxer-m <132169076+Not-muzzyy@users.noreply.github.com>
2024-05-04 16:22:00 +0530

    Update config.py

--- Vizxer-m/config.py
+++ Vizxer-m/config.py
@@ -6,14 +6,14 @@ class Config(object):
     sudo_users = "7011799629", "6396361171"
     GROUP_ID = -1002145420408
     TOKEN = "6889782542:AAFfQ13Rji_eDZkysaxHeEJTWdfjsDuNPts"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
+    mongo_url = "mongodb+srv://ichigobotcloud:GHGfLff2QFlT4sMM@aryanzxa.0r5vjie.mongodb.net/?retryWrites=true&w=majority&appName=aryanzxa"
     PHOTO_URL = ["https://telegra.ph/file/b8232e33e2e1d09c5687f.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = ""
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    SUPPORT_CHAT = "Rulers_Bots_Support"
+    UPDATE_CHAT = "waifuobtainers"
+    BOT_USERNAME = "Waifu_obtainerbot"
+    CHARA_CHANNEL_ID = "-1002071072596"
+    api_id = 19869418
+    api_hash = "c69a930758c50d764ad678b84c097a55"
 
     
 class Production(Config):
