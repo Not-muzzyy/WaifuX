28b375e3b0f165e94f69c502b5a7607f68c02d01
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-05-04 18:18:28 +0530

    Update config.py

--- Vizxer/config.py
+++ Vizxer/config.py
@@ -6,7 +6,7 @@ class Config(object):
     sudo_users = "7011799629", "6396361171"
     GROUP_ID = -1002145420408
     TOKEN = "6889782542:AAFfQ13Rji_eDZkysaxHeEJTWdfjsDuNPts"
-    mongo_url = "mongodb+srv://ichigobotcloud:GHGfLff2QFlT4sMM@aryanzxa.0r5vjie.mongodb.net/?retryWrites=true&w=majority&appName=aryanzxa"
+    mongo_url = "mongodb+srv://edit72160:edit72160@cluster0.aceg9xy.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b8232e33e2e1d09c5687f.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "Rulers_Bots_Support"
     UPDATE_CHAT = "waifuobtainers"
