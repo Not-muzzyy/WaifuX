de5357a3568eaf4be8ad0911ae698d2d3874a936
ZEROTH LORD <140504665+ZEROTH-LORD@users.noreply.github.com>
2024-05-06 04:45:13 -0700

    Update config.py

--- Vizxer/config.py
+++ Vizxer/config.py
@@ -9,8 +9,8 @@ class Config(object):
     mongo_url = "mongodb+srv://edit72160:edit72160@cluster0.aceg9xy.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b8232e33e2e1d09c5687f.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "Rulers_Bots_Support"
-    UPDATE_CHAT = "waifuobtainers"
-    BOT_USERNAME = "Waifu_obtainerbot"
+    UPDATE_CHAT = "Rulers_Bots"
+    BOT_USERNAME = "Attain_Your_Waifu_Bot"
     CHARA_CHANNEL_ID = "-1002145420408"
     api_id = 19869418
     api_hash = "c69a930758c50d764ad678b84c097a55"
